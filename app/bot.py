3401c64fc8d5615a03abe46673a5d8a1221e1c55
Cameron Battagler <cameron@battagler.me>
2024-01-09 12:53:18 -0600

    Full rewrite for langchain 0.1.0

--- app/bot.py
+++ app/bot.py
@@ -1,28 +1,29 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 import os
-import re
 import asyncio
-import random
-import requests
-import json
+
+from operator import itemgetter
+
 from concurrent.futures import ThreadPoolExecutor
-from langchain.chat_models import AzureChatOpenAI
-from langchain.utilities import BingSearchAPIWrapper
+from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.memory import CosmosDBChatMessageHistory
-from langchain.agents import ConversationalChatAgent, AgentExecutor, Tool
 from typing import Any, Dict, List, Optional, Union
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.callbacks.manager import CallbackManager
-from langchain.schema import AgentAction, AgentFinish, LLMResult
+from langchain.schema import AgentAction
 
-from langchain.globals import set_verbose
-set_verbose(True)
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain.docstore.document import Document
+
+from langchain.globals import set_debug
+set_debug(True)
 
 #custom libraries that we will use later in the app
-from prompts import WELCOME_MESSAGE, CUSTOM_CHATBOT_PREFIX, CUSTOM_CHATBOT_SUFFIX
-from utils import DocSearchAgent, ChatGPTTool, BingSearchAgent, run_agent, model_tokens_limit
+from utils import AzureAiSearchIndexSchema, BlobStorageProperties, create_service_sas_blob
 
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount, Activity, ActivityTypes
@@ -33,6 +34,13 @@ load_dotenv()
 # Env variables needed by langchain
 os.environ["OPENAI_API_VERSION"] = os.environ.get("AZURE_OPENAI_API_VERSION")
 
+# Env Var to override default fields for Azure Search because langchain doesn't let you set them via API
+os.environ["AZURESEARCH_FIELDS_CONTENT"] = "chunk"
+os.environ["AZURESEARCH_FIELDS_CONTENT_VECTOR"] = "chunkVector"
+
+from langchain_community.vectorstores.azuresearch import AzureSearch
+
+EXPERTISE = os.environ["EXPERTISE_DESCRIPTION"]
 
 # Callback hanlder used for the bot service to inform the client of the thought process before the final response
 class BotServiceCallbackHandler(BaseCallbackHandler):
@@ -62,6 +70,14 @@ class MyBot(ActivityHandler):
     
     # Function to show welcome message to new users
     async def on_members_added_activity(self, members_added: ChannelAccount, turn_context: TurnContext):
+        WELCOME_MESSAGE = """
+Hello and welcome! \U0001F44B
+
+I'm a smart virtual assistant designed to help you find answers to your questions!
+
+Feel free to ask me anything about """ + EXPERTISE + """ and I'll do my best to answer it for you.
+"""
+
         for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity(WELCOME_MESSAGE)
@@ -80,45 +96,118 @@ class MyBot(ActivityHandler):
 
         # Setting the query to send to OpenAI
         input_text = turn_context.activity.text + "\n\n metadata:\n" + str(input_text_metadata)    
-            
-        # Set Callback Handler
-        cb_handler = BotServiceCallbackHandler(turn_context)
-        cb_manager = CallbackManager(handlers=[cb_handler])
 
-        # Set LLM 
-        llm = AzureChatOpenAI(deployment_name=self.model_name, temperature=0.5, max_tokens=1000, callback_manager=cb_manager)
+        embeddings: AzureOpenAIEmbeddings = AzureOpenAIEmbeddings(
+            azure_deployment=os.environ['AZURE_OPENAI_EMBEDDING_DEPLOYMENT'],
+            chunk_size=1,
+            azure_endpoint=os.environ['AZURE_OPENAI_ENDPOINT']
+        )
 
-        # Initialize our Tools/Experts
-        text_indexes = ["cogsrch-index-files"]
-        doc_search = DocSearchAgent(llm=llm, vector_only_indexes=text_indexes,
-                           k=5, similarity_k=4, reranker_th=1,
-                           callback_manager=cb_manager, return_direct=True)
-        www_search = BingSearchAgent(llm=llm, k=5, callback_manager=cb_manager, return_direct=True)
-        chatgpt_search = ChatGPTTool(llm=llm, callback_manager=cb_manager, return_direct=True)
+        # We need to define our Azure Search schema for Langchain to understand how to search
 
+        vector_store: AzureSearch = AzureSearch(
+            azure_search_endpoint=os.environ['AZURE_SEARCH_ENDPOINT'],
+            azure_search_key=os.environ['AZURE_SEARCH_KEY'],
+            index_name="cogsrch-index-files",
+            embedding_function=embeddings.embed_query,
+            fields=AzureAiSearchIndexSchema(),
+            semantic_configuration_name="openai-vectordb-semantic-configuration"
+        )
 
-        tools = [www_search, doc_search, chatgpt_search]
+        retriever = vector_store.as_retriever()
+
+        model_name = os.environ.get("AZURE_OPENAI_MODEL_NAME")
+
+        llm = AzureChatOpenAI(deployment_name=model_name, temperature=0.5, max_tokens=2000)
 
         # Set brain Agent with persisten memory in CosmosDB
-        cosmos = CosmosDBChatMessageHistory(
-                        cosmos_endpoint=os.environ['AZURE_COSMOSDB_ENDPOINT'],
-                        cosmos_database=os.environ['AZURE_COSMOSDB_NAME'],
-                        cosmos_container=os.environ['AZURE_COSMOSDB_CONTAINER_NAME'],
-                        connection_string=os.environ['AZURE_COMOSDB_CONNECTION_STRING'],
-                        session_id=session_id,
-                        user_id=user_id
-                    )
-        cosmos.prepare_cosmos()
+        # cosmos = CosmosDBChatMessageHistory(
+        #                 cosmos_endpoint=os.environ['AZURE_COSMOSDB_ENDPOINT'],
+        #                 cosmos_database=os.environ['AZURE_COSMOSDB_NAME'],
+        #                 cosmos_container=os.environ['AZURE_COSMOSDB_CONTAINER_NAME'],
+        #                 connection_string=os.environ['AZURE_COMOSDB_CONNECTION_STRING'],
+        #                 session_id=session_id,
+        #                 user_id=user_id
+        #             )
+        # cosmos.prepare_cosmos()
 
-        memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True, k=5, chat_memory=cosmos, max_token_limit=1000)
-        agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools,system_message=CUSTOM_CHATBOT_PREFIX,human_message=CUSTOM_CHATBOT_SUFFIX)
-        agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, memory=memory, handle_parsing_errors=True)
+        # memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True, k=5, chat_memory=cosmos, max_token_limit=1000)
+
+        prompt_topic = EXPERTISE
+
+        latent_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", "You're an assistant who's good at " + prompt_topic + " and based on the question provided, you will provide 10 reworded questions that help answer the original question."),
+                ("human", "{question}"),
+            ]
+        )
+
+        latent_chain = (
+            {"question": RunnablePassthrough()}
+            | latent_prompt
+            | llm
+            | StrOutputParser()
+        )
+
+        latent_condensed_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", "You're an assistant who's good at " + prompt_topic + " and based on the original question and new questions provided, provide the best possible worded question to get the original questions answer."),
+                ("human", """## original question: 
+                 
+                 {question}
+                 
+
+                 ### new questions:
+
+                 {questions}"""),
+            ]
+        )
+
+        latent_condensed_chain = (
+            {"question": RunnablePassthrough(), "questions": latent_chain}
+            | latent_condensed_prompt
+            | llm
+            | StrOutputParser()
+        )
+
+        template = """You're an assistant who's good at """ + prompt_topic + """. Answer the question and provide more details based only on the following context sources are provided in the location metadata field:
+        {context}
+
+        Question: {generated_question}
+
+        You will get 40 points if you include the source of your answer with it's full url including parameters in a markdown link
+        You will get 30 more points if you use markdown to format your answer in a pleasing manner
+        """
+        prompt = ChatPromptTemplate.from_template(template)
+
+        blob_storage = BlobStorageProperties()
+
+        def _add_sas_urls(documents: List[Document]) -> List[Document]:
+            for doc in documents:
+                full_blob_name = doc.metadata['location'].split(blob_storage.container_name+"/")[1]
+                sas_key = create_service_sas_blob(full_blob_name, blob_storage)
+                doc.metadata["location"] = doc.metadata["location"] + "?" + sas_key
+            return documents
+
+        _context = {
+            "context": latent_condensed_chain | retriever | _add_sas_urls,
+            "generated_question": latent_condensed_chain
+        }
+
+        chain = (
+            _context
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+
+        seq_chain = latent_chain | latent_condensed_chain | chain
 
         await turn_context.send_activity(Activity(type=ActivityTypes.typing))
         
         # Please note below that running a non-async function like run_agent in a separate thread won't make it truly asynchronous. It allows the function to be called without blocking the event loop, but it may still have synchronous behavior internally.
         loop = asyncio.get_event_loop()
-        answer = await loop.run_in_executor(ThreadPoolExecutor(), run_agent, input_text, agent_chain)
+        answer = await loop.run_in_executor(ThreadPoolExecutor(), seq_chain.invoke, input_text)
         
         await turn_context.send_activity(answer)
 
